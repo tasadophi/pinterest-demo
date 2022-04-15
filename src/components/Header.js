7145d845ce90347deb00083af66aba31ad275ab4
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-16 01:48:38 +0430

    fix paddings

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import pinterest from "./../images/pinterestLogo.png";
 const Header = () => {
   return (
-    <header className="p-6 flex gap-4 items-center">
+    <header className="p-4 flex gap-4 items-center justify-between">
       <div className="flex gap-1 items-center">
         <img className="w-8 h-8" src={pinterest} alt="pinterest" />
         <span className="text-red-600 font-bold text-lg">Pinterest</span>
       </div>
       <div className="flex items-center gap-1">
-        <span className="font-bold p-3 rounded-full cursor-pointer hover:bg-gray-100">
+        <span className="font-bold p-2 rounded-full cursor-pointer hover:bg-gray-100">
           Explore
         </span>
-        <span className="font-bold p-3 rounded-full cursor-pointer hover:bg-gray-100">
+        <span className="font-bold p-2 rounded-full cursor-pointer hover:bg-gray-100">
           Get App
         </span>
-        <span className="font-bold p-3 rounded-full cursor-pointer hover:bg-gray-100">
+        <span className="font-bold p-2 rounded-full cursor-pointer hover:bg-gray-100">
           Login
         </span>
-        <span className="font-bold bg-gray-100 p-4 cursor-pointer rounded-full">
+        <span className="font-bold bg-gray-100 p-3 cursor-pointer rounded-full">
           Sign Up
         </span>
       </div>
