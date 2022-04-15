b84df77154dc37e58a7e918b68bc33e77021f283
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-16 01:50:34 +0430

    margin added

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -1,8 +1,8 @@
 const Middle = () => {
   return (
-    <div>
+    <div className="mb-4">
       <h1 className="text-center font-bold text-4xl">Handbags</h1>
-      <p className="p-2 text-center">
+      <p className="p-4 text-center">
         The perfect handbags from totes, cross-body bags, satchels to clutches
         to complement your wardrobe (and beyond).·522 Pins 4y
       </p>
