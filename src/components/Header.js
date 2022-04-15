f65307cc57627052d72c85aea6bf2e3ccd265261
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-16 01:05:55 +0430

    added cursor pointer

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,9 @@ const Header = () => {
         <span className="font-bold p-3 rounded-full cursor-pointer hover:bg-gray-100">
           Login
         </span>
-        <span className="font-bold bg-gray-100 p-4 rounded-full">Sign Up</span>
+        <span className="font-bold bg-gray-100 p-4 cursor-pointer rounded-full">
+          Sign Up
+        </span>
       </div>
     </header>
   );
