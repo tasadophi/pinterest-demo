15a6ec84841268066fb264c93495cb2891897f6a
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-16 01:12:35 +0430

    middle section created

---
+++ src/components/Middle.js
@@ -0,0 +1,13 @@
+const Middle = () => {
+  return (
+    <div>
+      <h1 className="text-center font-bold text-4xl">Handbags</h1>
+      <p className="p-2 text-center">
+        The perfect handbags from totes, cross-body bags, satchels to clutches
+        to complement your wardrobe (and beyond).·522 Pins 4y
+      </p>
+    </div>
+  );
+};
+
+export default Middle;
