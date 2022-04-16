89fdeb585cb546485ce451ddd2c0f607b43842a1
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-16 20:08:03 +0430

    fix font size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import pinterest from "./../images/pinterestLogo.png";
 const Header = () => {
   return (
-    <header className="p-4 flex gap-4 items-center justify-between">
+    <header className="p-4 flex gap-4 items-center justify-between text-sm sm:text-lg">
       <div className="flex gap-1 items-center">
-        <img className="w-8 h-8" src={pinterest} alt="pinterest" />
-        <span className="text-red-600 font-bold text-lg">Pinterest</span>
+        <img className="w-6 h-6" src={pinterest} alt="pinterest" />
+        <span className="text-red-600 font-bold">Pinterest</span>
       </div>
       <div className="flex items-center gap-1">
         <span className="font-bold p-2 rounded-full cursor-pointer hover:bg-gray-100">
