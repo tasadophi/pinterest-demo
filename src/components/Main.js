d63e18e53c8f2964147d627d0e1370560fbf9684
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-17 15:47:11 +0430

    main conponent completed and use infinite scroll

---
+++ src/components/Main.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import getProducts from "../services/getProducts";
+import Product from "./Product";
+import Masonry from "react-masonry-css";
+import InfiniteScroll from "react-infinite-scroller";
+
+const Loading = () => {
+  return (
+    <p className="text-4xl text-center p-4 text-green-600">
+      Loading Products ...
+    </p>
+  );
+};
+
+const Main = () => {
+  const [products, setProducts] = useState(null);
+
+  // offset value
+  const getOffset = () => parseInt(localStorage.getItem("offset"));
+  const addOffset = () =>
+    localStorage.setItem(
+      "offset",
+      parseInt(localStorage.getItem("offset")) + 1
+    );
+
+  useEffect(() => {
+    document.title = "Main Page";
+    const get = async () => {
+      const data = await getProducts(1);
+      setProducts(data);
+    };
+    get();
+    // reset offset
+    return () => {
+      localStorage.setItem("offset", 1);
+    };
+  }, []);
+
+  const breakpointColumnsObj = {
+    default: 5,
+    1100: 4,
+    700: 3,
+    500: 2,
+  };
+
+  const loadProducts = () => {
+    addOffset();
+    const offset = getOffset();
+    const get = async () => {
+      const data = await getProducts(offset);
+      setProducts([...products, ...data]);
+    };
+    get();
+  };
+
+  if (!products) return <Loading />;
+
+  return (
+    <main className="p-4">
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={loadProducts}
+        hasMore={true}
+        loader={<Loading key={0} />}
+      >
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="flex w-auto justify-center"
+          columnClassName="bg-clip-padding"
+        >
+          {products.map((p) => (
+            <Product key={p.page_id} p={p} />
+          ))}
+        </Masonry>
+      </InfiniteScroll>
+    </main>
+  );
+};
+
+export default Main;
