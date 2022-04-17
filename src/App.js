1352b20be6118d1e7e408d10479b9b94c62e70fd
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-17 15:48:09 +0430

    all components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Middle from "./components/Middle";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Middle />
+      <Main />
     </div>
   );
 }
