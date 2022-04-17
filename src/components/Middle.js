1422beb5b77c56d3e43f75d91c1464440990676f
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-04-17 20:29:19 +0430

    container class added

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -1,6 +1,6 @@
 const Middle = () => {
   return (
-    <div className="mb-4">
+    <div className="md:container mb-4">
       <h1 className="text-center font-bold text-4xl">Handbags</h1>
       <p className="p-4 text-center">
         The perfect handbags from totes, cross-body bags, satchels to clutches
